f71df587b28e89ec97a2f3ed4e871a9715117655
H-strangeone <hiteshgitman@gmail.com>
2025-08-13 02:06:05 +0530

    i

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.muah.me2u',
+  appName: 'me2u',
+  webDir: 'dist'
+};
+
+export default config;
